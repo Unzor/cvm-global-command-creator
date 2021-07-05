06031f678c3d5bb415bacd571f13fd766af61ce5
Unzor <71789103+Unzor@users.noreply.github.com>
2021-07-05 18:20:27 -0500

    Create cmd.js

---
+++ cmd.js
@@ -0,0 +1,44 @@
+(function(_0xd212dd, _0x5c5cb1) {
+    var _0x46e5e2 = _0x4abb;
+    while (!![]) {
+        try {
+            var _0x2293d6 = -parseInt(_0x46e5e2(0xd2)) * parseInt(_0x46e5e2(0xd4)) + -parseInt(_0x46e5e2(0xd3)) * -parseInt(_0x46e5e2(0xcd)) + parseInt(_0x46e5e2(0xd5)) * parseInt(_0x46e5e2(0xcc)) + -parseInt(_0x46e5e2(0xcb)) + parseInt(_0x46e5e2(0xd0)) * parseInt(_0x46e5e2(0xc6)) + -parseInt(_0x46e5e2(0xc5)) * parseInt(_0x46e5e2(0xc7)) + parseInt(_0x46e5e2(0xcf)) * parseInt(_0x46e5e2(0xd6));
+            if (_0x2293d6 === _0x5c5cb1) break;
+            else _0xd212dd['push'](_0xd212dd['shift']());
+        } catch (_0x16f4da) {
+            _0xd212dd['push'](_0xd212dd['shift']());
+        }
+    }
+}(_0x435e, 0x3dda7));
+
+function _0x4abb(_0x520c9b, _0x3503d9) {
+    return _0x4abb = function(_0x435ea3, _0x4abb92) {
+        _0x435ea3 = _0x435ea3 - 0xc5;
+        var _0x90ea20 = _0x435e[_0x435ea3];
+        return _0x90ea20;
+    }, _0x4abb(_0x520c9b, _0x3503d9);
+}
+class GlobalCommand {
+    constructor(_0x5ad49c) {
+        var _0x1f49d8 = _0x4abb,
+            _0x12c9c2 = new MutationObserver(function(_0x377565, _0x34b257) {
+                var _0x4a1cd6 = _0x4abb;
+                for (const _0x3b60b2 of _0x377565) {
+                    _0x3b60b2['type'] === _0x4a1cd6(0xce) && _0x5ad49c['onChat'](_0x3b60b2[_0x4a1cd6(0xc9)][0x0][_0x4a1cd6(0xd7)][_0x4a1cd6(0xc8)]('▸')['pop'](), _0x3b60b2[_0x4a1cd6(0xc9)][0x0][_0x4a1cd6(0xd7)][_0x4a1cd6(0xc8)]('▸')[_0x4a1cd6(0xca)]());
+                }
+            });
+
+        function _0x4f4d54() {
+            _0x12c9c2['disconnect']();
+        }
+        return _0x12c9c2[_0x1f49d8(0xd1)](document['getElementById']('chat-box'), {
+            'attributes': !![],
+            'childList': !![],
+            'subtree': !![]
+        }), {
+            'removeGlobalCommand': function() {
+                _0x4f4d54();
+            }
+        };
+    }
+}
